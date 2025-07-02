1962d3cef947f2981d97081a85b8ad073b91eb9d
sowjanya <sowjanya.vm@excelindia.com>
2025-07-02 11:15:48 +0530

    Initial commit: Vite + React project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1 className='text-4xl font-bold text-center text-white bg-blue-600 p-4 rounded'>
+          Hello world
+        </h1>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
